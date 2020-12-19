9f5a11aec7bf469fd061826a0c14044c20d2c9d6
SHM <SHM@qq.com>
2020-12-19 21:28:08 +0800

    SQL文件

---
+++ db/t_lang.sql
@@ -0,0 +1,7 @@
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (1, '英语', 'en');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (2, '日语', 'jp');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (3, '俄语', 'ru');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (4, '法语', 'fra');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (5, '韩语', 'kor');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (6, '荷兰语', 'nl');
+INSERT INTO `t_lang` (`id`, `langName`, `langCode`) VALUES (7, '阿拉伯语', 'ara');
